b482a68e1a4eddb2ebc4219e1ff19fce102d63ef
Natalia Phipps <130694752+Natphipps@users.noreply.github.com>
2023-08-16 18:08:18 -0400

    Update data_analysis_employees.sql

--- employeeSQL/data_analysis_employees.sql
+++ employeeSQL/data_analysis_employees.sql
@@ -7,4 +7,4 @@ on employees.emp_no = salaries.emp_no;
 --List the first name, last name, and hire date for the employees who were hired in 1986
 select employees.first_name, employees.last_name, employees.hire_date
 from employees
-where hire_date >= 1/1/1986 AND <= 12/30/1968;
+where hire_date >= 1/1/1986 AND <= 12/30/1986;
